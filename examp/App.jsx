ec6758378e3ab10b6396ac59911e5923eb3e4812
allen9322 <56813525+allen9322@users.noreply.github.com>
2021-06-09 19:27:59 +0800

    exam

---
+++ examp/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!
+         </div>
+      );
+   }
+}
+export default App;
